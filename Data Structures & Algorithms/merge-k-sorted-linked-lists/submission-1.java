dad989daa797e6afae8388fe97b40c72cab78e9e
PandeyKishan <100477995+PandeyKishan@users.noreply.github.com>
2026-08-07 00:19:45 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0)
+            return null;
+    //     int n = lists.length;
+    //     int interval = 1;
+
+    //     while(interval < n) {
+    //         for(int i = 0; i + interval < n; i += interval * 2) {
+    //             lists[i] = mergeLists(lists[i], lists[i + interval]);
+    //         }
+    //         interval *= 2;
+    //     }
+
+    //     return lists[0];
+    // }
+
+    // public ListNode mergeLists(ListNode list1, ListNode list2) {
+    //     ListNode dummy = new ListNode();
+    //     ListNode curr = dummy;
+
+    //     ListNode ptr1 = list1;
+    //     ListNode ptr2 = list2;
+
+    //     while(ptr1 != null && ptr2 != null) {
+    //         if(ptr1.val <= ptr2.val) {
+    //             curr.next = ptr1;
+    //             ptr1 = ptr1.next;
+    //         } else {
+    //             curr.next = ptr2;
+    //             ptr2 = ptr2.next;
+    //         }
+    //         curr = curr.next;
+    //     }
+
+    //     if(ptr1 != null) {
+    //         curr.next = ptr1;
+    //         ptr1 = ptr1.next;
+    //         curr = curr.next;
+    //     } else {
+    //         curr.next = ptr2;
+    //         ptr2 = ptr2.next;
+    //         curr = curr.next;
+    //     }
+
+    //     return dummy.next;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for(ListNode node: lists) {
+            if(node != null)
+                pq.offer(node); 
+        }
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while(!pq.isEmpty()) {
+            ListNode smallest = pq.poll();
+            curr.next = smallest;
+            curr = curr.next;
+
+            if(smallest.next != null) {
+                pq.offer(smallest.next);
+            }
+        }
+        return dummy.next;
+    }
+}
